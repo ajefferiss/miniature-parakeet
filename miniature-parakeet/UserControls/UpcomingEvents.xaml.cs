fe1fa4c8529c5ca2d80aa8b46d45b68543a9eaff
Adam Jefferiss <adam.jefferiss@gmail.com>
2021-02-16 22:25:14 +0000

    Add user controls for month list, pager and upcoming

---
+++ miniature-parakeet/UserControls/UpcomingEvents.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace miniature_parakeet.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UpcomingEvents.xaml
+    /// </summary>
+    public partial class UpcomingEvents : UserControl
+    {
+        public UpcomingEvents()
+        {
+            InitializeComponent();
+
+            for (int x = 0; x < 50; x++)
+            {
+                ListViewItem listViewItem = new ListViewItem();
+                listViewItem.Content = "Hello " + x;
+                UpcomingPlannerItems.Items.Add(listViewItem);
+            }
+        }
+    }
+}
