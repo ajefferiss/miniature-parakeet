4f8d921f1f1031d91cf730ee8ca9b8fef36c1796
Adam Jefferiss <adam.jefferiss@gmail.com>
2021-02-16 22:25:52 +0000

    Include startup (for future pre-population)

--- miniature-parakeet/App.xaml.cs
+++ miniature-parakeet/App.xaml.cs
@@ -13,5 +13,12 @@ namespace miniature_parakeet
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.PopulateDemoPlanner();
+            window.Show();
+        }
+
     }
 }
